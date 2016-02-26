3acad710836b7f7b0c3c2cb714391aa3cd40e09c
Tauseef <Tauseef@10.146.224.209>
2016-02-26 17:32:05 -0600

    Figured stuff out for makeLadder and dictionary

--- src/assignment4/WordLadderSolver.java
+++ src/assignment4/WordLadderSolver.java
@@ -4,12 +4,15 @@
 
 package assignment4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // do not change class name or interface it implements
 public class WordLadderSolver implements Assignment4Interface
 {
-    // delcare class members here.
+	List<String> solutionList = new ArrayList<String>();
+	
+    // declare class members here.
 
     // add a constructor for this object. HINT: it would be a good idea to set up the dictionary there
 
@@ -17,8 +20,32 @@ public class WordLadderSolver implements Assignment4Interface
     @Override
     public List<String> computeLadder(String startWord, String endWord) throws NoSuchLadderException 
     {
+    	
         // implement this method
         throw new UnsupportedOperationException("Not implemented yet!");
+    }
+    
+    public boolean makeLadder(String fromWord, String toWord, int index)
+    {
+    	if(fromWord difference of one toWord)
+    		return true;
+    	
+    	solutionList.add(fromWord);
+    	//iterate through dictionary
+    	//find all difference of one words and put into templist
+    	
+    	for(int i = 0; i < tempList.size(); i++)
+    	{
+    		if(makeLadder(tempList.get(i), toWord, index++) == true)
+    		{
+    			return true;
+    		}
+    		else
+    			solutionList.remove(tempList.get(i))
+    	}
+    	
+    	return false;
+    	
     }
 
     @Override
